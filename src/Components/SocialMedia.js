19f8a8ec4b40c1f30ddc1b6938eca31059c2c50e
Osama <osamamegahed2468@gmail.com>
2022-09-30 22:43:04 +0200

    updated dark mode

--- src/Components/SocialMedia.js
+++ src/Components/SocialMedia.js
@@ -1,45 +1,19 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { BsInstagram, BsSun } from "react-icons/bs"
 import { FaFacebook } from "react-icons/fa"
 import { MdOutlineNightlight } from "react-icons/md"
 import { AiFillLinkedin, AiOutlineGithub } from "react-icons/ai"
 
+
 const NavigationDots = ({ isDark }) => {
-    function handleSystem() {
-        let is_copy_dark = !isDark
-        if (is_copy_dark) {
-            return
-        } else {
-
-            document.documentElement.style.setProperty('--backgroundColor', '#EDF2F8')
-            document.documentElement.style.setProperty('--secondary-color', '#fff')
-            document.documentElement.style.setProperty('--primaryColor', '#313BAC')
-            document.documentElement.style.setProperty('--gray-color', '#000')
-            document.documentElement.style.setProperty('--blackTech', '#fff')
-            document.documentElement.style.setProperty('--whiteColor', '#000')
-            document.documentElement.style.setProperty('--secondary-color-footer', '#313BAC')
-            document.documentElement.style.setProperty('--backgroundTable', '#fff')
-            document.documentElement.style.setProperty('--primaryColorTable', '#e4e4e4')
-            document.documentElement.style.setProperty('--darkNav', '#ffffff4a')
-            document.documentElement.style.setProperty('--borderDarkNav', '#a2bfce')
-            document.documentElement.style.setProperty('--navLinksColorDark', '#3AA6EF')
-            document.documentElement.style.setProperty('--navLinksColorDarkSocial', '#3AA6EF')
-            document.documentElement.style.setProperty('--DesignerTextDark', '#3AA6EF')
-            document.documentElement.style.setProperty('--tableBackgroundDark', '#3AA6EF')
-            document.documentElement.style.setProperty('--itemsFilteredColor', '#fff')
-            document.documentElement.style.setProperty('--itemsFilteredColorHover', '#3AA6EF')
-
-
-
-
-
-        }
+    const [Dark, setDark] = useState(true)
+    function handleDarkModeButton() {
+        let darkCopy = !Dark;
+        setDark(darkCopy)
     }
+    function handleDrakMode() {
 
-    function handleSystem2() {
-
-        if (isDark) {
-            // getComputedStyle(document.documentElement).getPropertyValue('--backgroundColor');
+        if (Dark) {
             document.documentElement.style.setProperty('--backgroundColor', '#162d53')
             document.documentElement.style.setProperty('--secondary-color', '#313bac')
             document.documentElement.style.setProperty('--primaryColor', '#7865FF')
@@ -57,18 +31,35 @@ const NavigationDots = ({ isDark }) => {
             document.documentElement.style.setProperty('--tableBackgroundDark', '#7865FF')
             document.documentElement.style.setProperty('--itemsFilteredColor', '#313bac')
             document.documentElement.style.setProperty('--itemsFilteredColorHover', '#7865FF')
-            
-
+        } else {
+            document.documentElement.style.setProperty('--backgroundColor', '#EDF2F8')
+            document.documentElement.style.setProperty('--secondary-color', '#fff')
+            document.documentElement.style.setProperty('--primaryColor', '#313BAC')
+            document.documentElement.style.setProperty('--gray-color', '#000')
+            document.documentElement.style.setProperty('--blackTech', '#fff')
+            document.documentElement.style.setProperty('--whiteColor', '#000')
+            document.documentElement.style.setProperty('--secondary-color-footer', '#313BAC')
+            document.documentElement.style.setProperty('--backgroundTable', '#fff')
+            document.documentElement.style.setProperty('--primaryColorTable', '#e4e4e4')
+            document.documentElement.style.setProperty('--darkNav', '#ffffff4a')
+            document.documentElement.style.setProperty('--borderDarkNav', '#a2bfce')
+            document.documentElement.style.setProperty('--navLinksColorDark', '#3AA6EF')
+            document.documentElement.style.setProperty('--navLinksColorDarkSocial', '#3AA6EF')
+            document.documentElement.style.setProperty('--DesignerTextDark', '#3AA6EF')
+            document.documentElement.style.setProperty('--tableBackgroundDark', '#3AA6EF')
+            document.documentElement.style.setProperty('--itemsFilteredColor', '#fff')
+            document.documentElement.style.setProperty('--itemsFilteredColorHover', '#3AA6EF')
         }
     }
+    useEffect(() => {
+        handleDrakMode()
+    }, [Dark])
+
     return (
         <div className="app__social">
 
-            <div className="system__icon system_sun" onClick={handleSystem}>
-                <button href="#" target="_blank" rel="noopener noreferrer" ><BsSun /> </button>
-            </div>
-            <div className="system__icon system_moon" onClick={handleSystem2}>
-                <button href="#" target="_blank" rel="noopener noreferrer" ><MdOutlineNightlight /></button>
+            <div className="system__icon system_sun" onClick={handleDarkModeButton}>
+                <button href="#" target="_blank" rel="noopener noreferrer" >{Dark ? <BsSun />  : <MdOutlineNightlight />}  </button>
             </div>
             <div className="linkedin__icon">
                 <a href="https://www.linkedin.com/in/osama-meaghed-58763022a/" target="_blank" rel="noopener noreferrer" ><AiFillLinkedin /></a>
